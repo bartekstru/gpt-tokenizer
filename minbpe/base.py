0b20873ce7a19e8632909d28eb67e4e6989adb13
bartekstru <bartek.stru@gmail.com>
2024-10-10 17:49:41 +0200

    Prettier code

--- minbpe/base.py
+++ minbpe/base.py
@@ -1,5 +1,6 @@
 import unicodedata
 
+
 class Tokenizer:
     """
     Base class for tokenizers.
@@ -21,7 +22,7 @@ class Tokenizer:
         Sets up empty merges, pattern, and special_tokens dictionaries,
         and initializes the vocabulary with basic byte representations.
         """
-        # default: vocab size of 256 (all bytes), no merges, no patterns
+        # Default: vocab size of 256 (all bytes), no merges, no patterns
         self.merges = {}  # (int, int) -> int
         self.pattern = ""  # str
         self.special_tokens = {}  # str -> int, e.g. {'<|endoftext|>': 100257}
@@ -41,7 +42,7 @@ class Tokenizer:
             NotImplementedError: This method should be implemented by subclasses.
         """
         raise NotImplementedError
-    
+
     def encode(self, text):
         """
         Encode the given text into token IDs.
@@ -56,7 +57,7 @@ class Tokenizer:
             NotImplementedError: This method should be implemented by subclasses.
         """
         raise NotImplementedError
-    
+
     def decode(self, ids):
         """
         Decode the given token IDs back into text.
@@ -82,14 +83,14 @@ class Tokenizer:
         Returns:
             dict: A dictionary mapping token IDs to their byte representations.
         """
-        # vocab is simply and deterministically derived from merges
+        # Vocab is simply and deterministically derived from merges
         vocab = {idx: bytes([idx]) for idx in range(256)}
         for (p0, p1), idx in self.merges.items():
             vocab[idx] = vocab[p0] + vocab[p1]
         for special, idx in self.special_tokens.items():
             vocab[idx] = special.encode("utf-8")
         return vocab
-    
+
     def save(self, file_prefix):
         """
         Save the tokenizer model to files.
@@ -101,39 +102,40 @@ class Tokenizer:
         Args:
             file_prefix (str): The prefix for the output files.
         """
-        # write the model: to be used in load() later
+        # Write the model: to be used in load() later
         model_file = file_prefix + ".model"
         with open(model_file, 'w') as f:
-            # write the version, pattern and merges, that's all that's needed
+            # Write the version, pattern and merges, that's all that's needed
             f.write("minbpe v1\n")
             f.write(f"{self.pattern}\n")
-            # write the special tokens, first the number of them, then each one
+            # Write the special tokens, first the number of them, then each one
             f.write(f"{len(self.special_tokens)}\n")
             for special, idx in self.special_tokens.items():
                 f.write(f"{special} {idx}\n")
-            # the merges dict
+            # The merges dict
             for idx1, idx2 in self.merges:
                 f.write(f"{idx1} {idx2}\n")
-        # write the vocab: for the human to look at
+
+        # Write the vocab: for the human to look at
         vocab_file = file_prefix + ".vocab"
         inverted_merges = {idx: pair for pair, idx in self.merges.items()}
         with open(vocab_file, "w", encoding="utf-8") as f:
             for idx, token in self.vocab.items():
-                # note: many tokens may be partial utf-8 sequences
+                # Note: many tokens may be partial utf-8 sequences
                 # and cannot be decoded into valid strings. Here we're using
                 # errors='replace' to replace them with the replacement char �.
-                # this also means that we couldn't possibly use .vocab in load()
+                # This also means that we couldn't possibly use .vocab in load()
                 # because decoding in this way is a lossy operation!
                 s = render_token(token)
-                # find the children of this token, if any
+                # Find the children of this token, if any
                 if idx in inverted_merges:
-                    # if this token has children, render it nicely as a merge
+                    # If this token has children, render it nicely as a merge
                     idx0, idx1 = inverted_merges[idx]
                     s0 = render_token(self.vocab[idx0])
                     s1 = render_token(self.vocab[idx1])
                     f.write(f"[{s0}][{s1}] -> [{s}] {idx}\n")
                 else:
-                    # otherwise this is leaf token, just print it
+                    # Otherwise this is leaf token, just print it
                     # (this should just be the first 256 tokens, the bytes)
                     f.write(f"[{s}] {idx}\n")
 
@@ -152,22 +154,22 @@ class Tokenizer:
                             or version.
         """
         assert model_file.endswith(".model")
-        # read the model file
+        # Read the model file
         merges = {}
         special_tokens = {}
         idx = 256
         with open(model_file, 'r', encoding="utf-8") as f:
-            # read the version
+            # Read the version
             version = f.readline().strip()
             assert version == "minbpe v1"
-            # read the pattern
+            # Read the pattern
             self.pattern = f.readline().strip()
-            # read the special tokens
+            # Read the special tokens
             num_special = int(f.readline().strip())
             for _ in range(num_special):
                 special, special_idx = f.readline().strip().split()
                 special_tokens[special] = int(special_idx)
-            # read the merges
+            # Read the merges
             for line in f:
                 idx1, idx2 = map(int, line.split())
                 merges[(idx1, idx2)] = idx
@@ -193,6 +195,7 @@ def count_pairs(tokens, counts=None):
         pairs[tokens[i], tokens[i+1]] = pairs.get((tokens[i], tokens[i+1]), 0) + 1
     return pairs
 
+
 def merge_tokens(tokens, pair: tuple, new_token: int):
     """
     Merge specified token pairs into a new token.
@@ -216,6 +219,7 @@ def merge_tokens(tokens, pair: tuple, new_token: int):
             i += 1
     return output
 
+
 def replace_control_characters(s: str) -> str:
     """
     Replace control characters in the given string with their escaped hexadecimal representation.
@@ -229,10 +233,11 @@ def replace_control_characters(s: str) -> str:
     chars = []
     for ch in s:
         if unicodedata.category(ch)[0] != "C":
-            chars.append(ch) # this character is ok
+            chars.append(ch)  # This character is ok
         else:
-            chars.append(f"\\u{ord(ch):04x}") # escape
+            chars.append(f"\\u{ord(ch):04x}")  # Escape
     return "".join(chars)
+
 
 def render_token(t: bytes) -> str:
     """
